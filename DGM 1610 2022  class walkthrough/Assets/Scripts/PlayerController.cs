080fe5d7d8d6c0c1f191558dfa6f40c9efb1e22e
Braxton Brown <10944268@uvu.edu>
2022-01-31 17:14:15 -0700

    Added player controller_Basic Movement

--- DGM 1610 2022  class walkthrough/Assets/Scripts/PlayerController.cs
+++ DGM 1610 2022  class walkthrough/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
+
 {
+    public  float speed = 20.0f;
+    public float turnspeed = 50.0f;
+
+    public float hImput;
+    public float fImput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      hImput = Input.GetAxis("Horizontal");
+
+      fImput = Input.GetAxis("Vertical");
+
+     // Translate(x,y,z) - Makes the player move forward // Time.DeltaTime make use of seconds and the * after control the speed or a variuble 
+     transform.Translate(Vector3.forward * Time.deltaTime * speed * fImput);
+
+     transform.Translate(Vector3.right * Time.deltaTime * turnspeed * hImput);
+
     }
 }
