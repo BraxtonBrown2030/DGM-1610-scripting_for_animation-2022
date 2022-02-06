fa0c36a319dd6eb0ad87bfc8ce343b9382427121
Braxton Brown <10944268@uvu.edu>
2022-02-06 15:59:54 -0700

    Code clean up for Player Controller / Camera Follow

--- DGM 1610 2022  class walkthrough/Assets/Scripts/CameraFollow.cs
+++ DGM 1610 2022  class walkthrough/Assets/Scripts/CameraFollow.cs
@@ -6,13 +6,12 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
 
-// (0,0,0) is the offset of tyhe camera
+// (0,0,0) is the offset of the camera relative to the target object (Player)
     private Vector3 offset = new Vector3(0,2,-3);
 
-    // Update is called once per frame
     void Update()
     {
-        //follows the player
+        //follows the target object (player)
         transform.position = player.transform.position + offset;
 
     }
--- DGM 1610 2022  class walkthrough/Assets/Scripts/PlayerController.cs
+++ DGM 1610 2022  class walkthrough/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-
-
 {
     private  float speed = 20.0f;
     private float turnspeed = 50.0f;
     private float hImput;
     private float fImput;
-    // Update is called once per frame
     void Update()
     {
       hImput = Input.GetAxis("Horizontal");
       fImput = Input.GetAxis("Vertical");
 
-     // Translate(x,y,z) - Makes the player move forward // Time.DeltaTime make use of seconds and the * after control the speed or a variuble 
+     // Translate(x,y,z) - Makes the player move forward // Time.DeltaTime make use of seconds and the * after control the speed or another variable
      transform.Translate(Vector3.forward * Time.deltaTime * speed * fImput);
      transform.Rotate(Vector3.up, turnspeed * Time.deltaTime * hImput);
 
