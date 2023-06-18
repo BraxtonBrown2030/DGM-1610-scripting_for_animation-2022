76594ed27ec5cb0682a1fa88d5ad1611bc52e43c
Braxton Brown <10944268@uvu.edu>
2023-06-17 22:54:11 -0600

    6/17/2023

--- Space shooter/Assets/Scripts/Main_Menu.cs
+++ Space shooter/Assets/Scripts/Main_Menu.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+
+    public int _MainMenu;
+
+
     // Start is called before the first frame update
-    void StartGame()
+    void Start()
     {
         
+     SceneManager.LoadScene(_MainMenu);
+     Debug.Log("load working");
+
     }
 
     // Update is called once per frame
-    void Quitgame()
+    void Quit()
     {
-        
+        Application.Quit();
+
     }
 }
