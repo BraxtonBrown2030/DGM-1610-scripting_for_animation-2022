6568d7833a42713f0c6c69a4051e0ffa468f8cb6
Braxton Brown <10944268@uvu.edu>
2023-07-06 18:25:49 -0600

    scriptable objects

---
+++ Space shooter/Assets/Scripts/ScriptableObjects/FloatData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float amount = 0f;
+
+    public void UpdateAmount(float other)
+    {
+        amount += other;
+    }
+}
