15335567f2ff838453ecb1676f043911cf3cc1b2
Braxton Brown <10944268@uvu.edu>
2023-07-11 21:32:05 -0600

    universal timer and scene save

---
+++ Prototype 2 - Ballon Pop Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Timer : ScriptableObject
+{
+   
+    public float timer = Time.time;
+
+
+    public void Update()
+    {
+
+        
+    }
+
+}
