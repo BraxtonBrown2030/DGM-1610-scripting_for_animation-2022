d8b7805ddd88dee0521ecb2d9670c195c5624123
Braxton Brown <10944268@uvu.edu>
2023-07-15 13:51:07 -0600

    Git ignore for text mesh pro and scene cleanup

--- Space shooter/Assets/Scripts/Enemy_Top.cs
+++ Space shooter/Assets/Scripts/Enemy_Top.cs
@@ -5,25 +5,20 @@ using UnityEngine;
 
 public class Enemy_Top : MonoBehaviour
 {
-
-    [Header("stats")]
+    [Header("Health")]
     public float health;
-    public float danamge;
-    public GameObject _lazer;
-    public float _UpSpeed = 1;
-    public bool antimation = false;
-    public Transform pointA;
-    public Transform pointB;
-    public float stopDistance;
-    public float speed = 2;
-    public float _Attackspeed = -0.5f;
-    public float _LastAttackTime = 0.3f ;
+    
+    private Transform player;
+    public float _StopDistance = 7;
+    public float _Speed = 2;
+    public GameObject _Laser;
+    public float _Attackspeed = -1f;
+    public float _LastAttackTime = 1f;
 
     void Start()
     {
-        pointA = GameObject.FindGameObjectWithTag("PointA").GetComponent<Transform>();
-        pointB = GameObject.FindGameObjectWithTag("PointB").GetComponent<Transform>();
         
+    player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 
     }
 
@@ -31,30 +26,17 @@ public class Enemy_Top : MonoBehaviour
     void Update()
     {
         
-
-        if(transform.position.y <= 5.5f)
+        if(Vector3.Distance(transform.position, player.position) > _StopDistance) // fllowing
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, _Speed * Time.deltaTime);
+        }
+        
+        if(transform.position.y <= 5.5f) // downward barrirer
         {
 
-            transform.Translate(Vector3.up * Time.deltaTime * _UpSpeed);
+            transform.position = new Vector3(transform.position.x,5.5f,0);
 
         }
-
-        if(Vector3.Distance(transform.position, pointA.position) == stopDistance)
-        {
-
-            transform.position = Vector3.MoveTowards(transform.position, pointB.position, speed * Time.deltaTime);
-
-            Debug.Log("movement works");
-
-        }
-        else if(Vector3.Distance(transform.position, pointB.position) == stopDistance)
-        {
-
-            transform.position = Vector3.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
-
-
-        }
-
 
         if( Time.time >= _Attackspeed)
         {
@@ -63,18 +45,41 @@ public class Enemy_Top : MonoBehaviour
 
         }
 
-
-
     }
-
 
     void Attack()
     {
- 
+
         _Attackspeed = Time.time + _LastAttackTime;
 
-        Instantiate(_lazer, transform.position + new Vector3(0,-0.8f,0), Quaternion.identity);
-
+        Instantiate(_Laser, transform.position + new Vector3(0,-0.8f,0), Quaternion.identity);
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("HIT:  " + other.transform.name);
+
+        if(other.tag == "Player")
+        {
+            Player_Movement player = other.transform.GetComponent<Player_Movement>();
+
+            if(player != null)
+            {
+                player.TakeDamage();
+            }
+
+        }
+
+        if(other.tag == "Lazer")
+        {
+
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
 }
