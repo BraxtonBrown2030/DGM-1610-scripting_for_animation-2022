960e6be6b570fc5b246b967660443449379bd81e
Braxton Brown <10944268@uvu.edu>
2023-07-16 19:09:39 -0600

    adding death counter and spawn conditions

---
+++ Space shooter/Assets/Scripts/Scriptibal objexts/Emeny_death.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Emeny_death : ScriptableObject
+{
+    
+    public float _NumberOfEnemyDeath;
+    public float fallDeath;
+    public float slidedeat;
+    public float topdeath;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        if()
+        {
+
+
+        }
+
+
+    }
+}
