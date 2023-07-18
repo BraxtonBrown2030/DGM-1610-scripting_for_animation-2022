16d652429356cdddc6b0376ac838b8f3594ae91f
Braxton Brown <10944268@uvu.edu>
2023-07-18 16:15:00 -0600

    Scene save 7/18/2023

--- Space shooter/Assets/Scripts/Spanw_Manager.cs
+++ Space shooter/Assets/Scripts/Spanw_Manager.cs
@@ -38,8 +38,6 @@ public class Spanw_Manager : MonoBehaviour
     void Start()
     {
 
-       //numberofenemys = GameObject.FindGameObjectsWithTag("Top_enemy" + "slide enemey" + "Fall enemey").Length;
-
         wavenumber = Time.time * wavenumber;
 
     }
